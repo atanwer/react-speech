21b8249eab4bcbcd620402efe5aa6186a35aec0c
Akalesh Tanwer <akalesh.aurasoft@gmail.com>
2023-07-19 20:25:38 +0530

    Removed paragraph

--- src/App.js
+++ src/App.js
@@ -33,8 +33,6 @@ const App = () => {
             <div className="container">
                 <h2>Speech to Text Converter</h2>
                 <br/>
-                <p>A React hook that converts speech from the microphone to text and makes it available to your React
-                    components.</p>
 
                 <div className="main-content" >
                     {transcript}
